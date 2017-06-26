631104c6a02be2273735dfe4af0e7efb4ddeaa7b
dennisknutsson <dennisknutsson@users.noreply.github.com>
2017-06-26 21:27:31 +0200

    Update setup_hass_centos7.sh

--- setup_hass_centos7.sh
+++ setup_hass_centos7.sh
@@ -1,13 +1,11 @@
+#!/bin/bash
 #https://github.com/pi-hole/pi-hole/blob/master/automated%20install/basic-install.sh
-
-Install homeassistant 
-
-Minimal
+#Install homeassistant 
+#on Centos7 Minimal
 
 yum install centos-release-scl -y
 yum install rh-python35 -y
 yum install gcc -y
-
 yum update -y
 yum upgrade -y
 yum install python-pip python3-dev -y
@@ -30,5 +28,5 @@ pip3 install --upgrade homeassistant
 
 systemctl disable firewalld
 
-(firewall-cmd --permanent --add-port=8123/tcp)
-(firewall-cmd --reload)
+firewall-cmd --permanent --add-port=8123/tcp
+firewall-cmd --reload
